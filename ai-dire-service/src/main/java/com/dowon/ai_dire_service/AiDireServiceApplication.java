a24e8a9ca1328785e4d02af1b7a3f2745874dc2e
KimDowon0419 <76195470+KimDowon0419@users.noreply.github.com>
2024-11-22 11:23:53 +0900

    init push

---
+++ ai-dire-service/src/main/java/com/dowon/ai_dire_service/AiDireServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dowon.ai_dire_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDireServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDireServiceApplication.class, args);
+	}
+
+}
